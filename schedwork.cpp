abdd462d7c22d5d038a119169511ca53bb0e2360
Vaishnavi Kolluru <vkolluru@usc.edu>
2025-04-21 02:48:29 +0000

    yayy

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,9 +21,53 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool assignment(const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<int>& shifts,
+    size_t myDay,
+    size_t mySlot);
 
 // Add your implementation of schedule() and other helper functions here
+bool assignment(const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<int>& shifts,
+    size_t myDay,
+    size_t mySlot) {
+        // total workers and days initialize
+        size_t workers = avail[0].size();
+        size_t days = avail.size();
+        
+        //all days scheduled
+        if (myDay==days) {
+            return true;
+        }
+        // all slots fileld already
+        if (mySlot==dailyNeed) {
+            return assignment(avail,dailyNeed,maxShifts,sched,shifts,myDay+1,0);
+        }
+        // see each worker to assign to slot or day
+        for (Worker_T i=0;i<workers;++i) {
+            if (avail[myDay][i]&&shifts[i]<static_cast<int>(maxShifts)) {
+                if (find(sched[myDay].begin(),sched[myDay].end(),i)!=sched[myDay].end()) {
+                    continue; // skip worker if alr assigned
+                }
+                sched[myDay][mySlot]=i;
+                shifts[i]++;// assign and increment
+                //recursion
+                if (assignment(avail,dailyNeed,maxShifts,sched,shifts,myDay,mySlot+1)) {
+                    return true;
+                }
+                sched[myDay][mySlot]=INVALID_ID;
+                shifts[i]--;// backtracking step
+
+            }
+        }
+        return false;// no worker found
+    }
 
 bool schedule(
     const AvailabilityMatrix& avail,
@@ -37,6 +81,18 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    size_t workers = avail[0].size();// workers total
+    size_t days = avail.size();// days total
+
+    // initialize schedule with empty
+    sched=DailySchedule(days,std::vector<Worker_T>(dailyNeed,INVALID_ID));
+    // shift count vector to track for each worker
+    vector<int> shiftCount(workers,0);
+    // recursion step
+    return assignment(avail,dailyNeed,maxShifts,sched,shiftCount,0,0);
+
+
+    
 
 
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void wordleHelper(string curr, int index, string floatingStr, const set<string>& myWords, set<string>& myFinds);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +22,74 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    set<string> myFinds;
+    wordleHelper(in,0,floating,dict, myFinds);
+    return myFinds;
 
 }
 
 // Define any helper functions here
+void wordleHelper(string curr, int index, string floatingStr, const set<string>& myWords, set<string>& myFinds) {
+    //unsigned int myInt = (unsigned int)index;
+    // base case word is full
+    if ((unsigned int)index == curr.size()) {
+        string myTemp = curr;
+        // check for all floating letters
+        for (unsigned int i = 0; i < floatingStr.size(); i++) {
+            size_t loco = myTemp.find(floatingStr[i]);
+            if (loco==string::npos) {
+                return; // reached end of str
+            }
+            // remove used character
+            myTemp.erase(loco,1);
+        }
+        // add word to results if in dictionary
+        if (myWords.find(curr) != myWords.end()) {
+            myFinds.insert(curr); 
+        }
+        return;
+    }
+
+    //if known char
+    if (!(curr[index]=='-')) {
+        // recursive step
+        wordleHelper(curr,index+1,floatingStr,myWords,myFinds);
+
+    }
+    else {
+        // non base case
+        int dash = 0;
+        for (unsigned int i = (unsigned int )index; i<curr.size();++i) {
+          if (curr[i]=='-') {
+            dash++; // iterate the dashes 
+          }
+        }
+        // not enough dashes, need to backtrack
+        if ((int)floatingStr.size()>dash) {return;}
+        // dashes=floating chars
+        if ((int)floatingStr.size()==dash) {
+          for (unsigned int i=0; i<floatingStr.size();++i) {
+            char c=floatingStr[i];
+            curr[index]=c; // use floating char
+            string newF=floatingStr;
+            newF.erase(i,1);  // remove used char 
+            wordleHelper(curr,index+1,newF,myWords,myFinds);
+          }
+        }
+        else {
+          // loop thru the alphabet
+          for (char c='a';c<='z';++c) {
+            curr[index]=c;
+            string newF=floatingStr;
+            size_t loc=newF.find(c);
+            // if in floating, remove letter
+            if (loc!=string::npos) {
+              newF.erase(loc,1);
+            }
+            // recursion
+            wordleHelper(curr,index+1,newF,myWords,myFinds);
+          }
+        }
+        
+    }
+}
